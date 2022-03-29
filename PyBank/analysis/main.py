1702ee6be46bc262b1ed3f1257b1957ad6d60bd2
Stefanie Gagnon <stefaniekathryn@gmail.com>
2022-03-29 11:56:05 -0400

    First py update to file, just started, testing to make sure works, more to come

--- PyBank/analysis/main.py
+++ PyBank/analysis/main.py
@@ -1,2 +1,15 @@
 import os
 import csv
+
+budget_data_csv = os.path.join("..", "Resources", "budget_data.csv")
+
+with open(budget_data_csv) as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=",")
+
+    
+    
+    
+   # print("Financial Analysis")
+   # print("----------------------------")
+
+
