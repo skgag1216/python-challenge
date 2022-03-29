d77dbf0dddbc8349e6d54305b8e5e76debc1a2ed
Stefanie Gagnon <stefaniekathryn@gmail.com>
2022-03-29 17:00:05 -0400

    First few changes to main.py and got some code working and running!

--- PyBank/analysis/main.py
+++ PyBank/analysis/main.py
@@ -1,15 +1,15 @@
 import os
 import csv
 
-budget_data_csv = os.path.join("..", "Resources", "budget_data.csv")
+budget_data_csv = ("Resources\\budget_data.csv")
 
-with open(budget_data_csv) as csvfile:
-    csv_reader = csv.reader(csvfile, delimiter=",")
-
+with open(budget_data_csv) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    csv_header = next(csv_file) 
+    print(f"Header: {csv_header}")
+    # for row in csv_reader:
+    #     line_count = 0
+    # if line_count == 0:
+    #     print(f'Column names are {(row[0])}')
+    #     line_count += 1
     
-    
-    
-   # print("Financial Analysis")
-   # print("----------------------------")
-
-
