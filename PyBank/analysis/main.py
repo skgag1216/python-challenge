b0f578238bd1b3f93f31cf33c37f30841fd1db7b
Stefanie Gagnon <stefaniekathryn@gmail.com>
2022-04-01 20:12:46 -0400

    Done!, have to export as txt file

--- PyBank/analysis/main.py
+++ PyBank/analysis/main.py
@@ -1,7 +1,10 @@
 import os
 import csv
+from numpy import maximum
 
-bd_csv = ("PyBank\\Resources\\budget_data.csv")
+from sympy import Max 
+
+bd_csv = ("Resources\\budget_data.csv")
 with open(bd_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     csv_header = next(csv_file) 
@@ -15,28 +18,26 @@ with open(bd_csv) as csv_file:
 
     for row in csv_reader:
         if month > 0:
-            current_pl = int(row[1])-previous_pl
+            current_pl = int(row[1])-previous_pl 
             overall_change.append(current_pl)
             if current_pl > greatest_inc:
                 greatest_inc = current_pl
-                print("Greatest inc is: " + str(row[0]))
+                greatest_inc_date = str(row[0]) 
             if current_pl < greatest_dec:
                 greatest_dec = current_pl
-                print("Greatest dec is: " + str(row[0]))
+                greatest_dec_date = str(row[0])
+                
         month += 1
         #print(type(row[1])) reading data type 
         net_budget += int(row[1])
         previous_pl = int(row[1])
-
 average_change = sum(overall_change)/len(overall_change)
 #print(sum(overall_change)/len(overall_change))
-
-#print(row.index(greatest_inc)) 
 
 print("Financial Analysis")
 print("-----------------------")
 print("Total months: " + str(month))
 print("Total net of Profits/Losses: " + "$" + str(net_budget))
 print("Average change: " +"$" + str(round(average_change, 2)))
-print("Greatest increase in profits: " + "$" + str(greatest_inc))
-print("Greatest decrease in profits: " + "$" + str(greatest_dec))
+print("Greatest increase in profits: " + greatest_inc_date + " $" + str(greatest_inc))
+print("Greatest decrease in profits: " + greatest_dec_date + " $" + str(greatest_dec))
