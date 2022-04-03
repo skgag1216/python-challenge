a47681c1086b282bf7721d12b7a78b0e5b4ef3f9
Stefanie Gagnon <stefaniekathryn@gmail.com>
2022-04-03 10:57:01 -0400

    Got main.py for PyPoll up and running, total votes done

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,13 +1,19 @@
-import os
+import os 
 import csv 
-
-election_csv = ("PyPoll\Resources\election_data.csv")
+election_csv = ("Resources\election_data.csv")
 with open(election_csv) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     csv_header = next(csv_file) 
 
     total_votes = 0
+    candidates = []
     for row in csv_reader:
         total_votes += 1
+        #candidate = str(row[2])
+        #candidates.append(candidate)
+        #candidates.sort()
 
-print("Total Votes:" + str(total_votes))
+L0 = "Election Results"
+L1 = "-----------------------"       
+L2 = ("Total Votes:" + str(total_votes))
+print(L2)
