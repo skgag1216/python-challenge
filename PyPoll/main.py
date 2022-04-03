cb6441f48663575e80178ab6f8563cc35355ad65
Stefanie Gagnon <stefaniekathryn@gmail.com>
2022-04-03 16:34:02 -0400

    worked thru percent of votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -19,7 +19,14 @@ with open(election_csv) as csv_file:
             candidates.append(candidate)
             votes_per_cand.append(1)
 
+#percent_of_vote = ((votes_per_cand/total_votes)*100)
+
 L0 = "Election Results"
 L1 = "-----------------------"       
 L2 = ("Total Votes:" + str(total_votes))
-print(L2)
+L2n = "-----------------------"
+L3 = (str(candidates[0]) + ": " + str(votes_per_cand[0]) + " " + str(((votes_per_cand[0])/total_votes)*100))
+L4 = (str(candidates[1]) + ": " + str(votes_per_cand[1]) + " " + str(((votes_per_cand[1])/total_votes)*100))
+L5 = (str(candidates[2]) + ": " + str(votes_per_cand[2]) + " " + str(((votes_per_cand[2])/total_votes)*100))
+lastL = "\n"+L0+"\n"+L1+"\n"+L2+"\n"+L2n+"\n"+L3+"\n"+L4+"\n"+L5+"\n"
+print(lastL)
