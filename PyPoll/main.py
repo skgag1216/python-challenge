7a77a0b35916fa3ba606c29b14c8eaf0255994bc
Stefanie Gagnon <stefaniekathryn@gmail.com>
2022-04-03 12:28:07 -0400

    updating code, trying to pull candidate names from appended candidate list

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,11 +7,17 @@ with open(election_csv) as csv_file:
 
     total_votes = 0
     candidates = []
+    votes_per_cand = []
     for row in csv_reader:
         total_votes += 1
-        #candidate = str(row[2])
-        #candidates.append(candidate)
+        candidate = str(row[2])
         #candidates.sort()
+        if candidate in candidates:
+            i = candidates.index(candidate)
+            votes_per_cand[i] += 1 
+        else:
+            candidates.append(candidate)
+            votes_per_cand.append(1)
 
 L0 = "Election Results"
 L1 = "-----------------------"       
